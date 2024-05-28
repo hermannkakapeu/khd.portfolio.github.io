825b70d1825bfeb758da7f3f53985bf2d5f48d6c
Hermann KAKPEU <hermann.kakpeu@centrale-casablanca.ma>
2024-05-28 01:34:49 +0000

    add images

---
+++ assets/js/popup.js
@@ -0,0 +1,49 @@
+let pop = document.getElementById('popup')
+// Function to open the popup
+function openPopup() {
+    pop.style.display = 'block';
+    pop.style.visibility = 'visible';
+}
+
+function closePopup() {
+    pop.style.visibility = 'hidden';
+}
+
+
+
+
+let popdash1 = document.getElementById('pop1')
+// Function to open the popup
+function openPopup1() {
+    popdash1.style.display = 'block';
+    popdash1.style.visibility = 'visible';
+}
+
+function closePopup1() {
+    popdash1.style.visibility = 'hidden';
+}
+
+
+
+let popdash2 = document.getElementById('pop2')
+// Function to open the popup
+function openPopup2() {
+    popdash2.style.display = 'block';
+    popdash2.style.visibility = 'visible';
+}
+
+function closePopup2() {
+    popdash2.style.visibility = 'hidden';
+}
+
+
+let popdash3 = document.getElementById('pop3')
+// Function to open the popup
+function openPopup3() {
+    popdash3.style.display = 'block';
+    popdash3.style.visibility = 'visible';
+}
+
+function closePopup3() {
+    popdash3.style.visibility = 'hidden';
+}
